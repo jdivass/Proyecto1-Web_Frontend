931f1a75fe194443028a0e4042b37eff6796e25b
div468 <julianandre.divas@gmail.com>
2026-04-23 17:17:04 -0600

    feat: correct loadSeries and renderSerie functions added

--- index.js
+++ index.js
@@ -4,10 +4,47 @@ var searchTerm = ""
 var sortAtribute = "id"
 var orderMethod = "desc"
 
+const loadSeries = async () => {
+    try {
+        const response = await getSeries(actualPage, limitPage, searchTerm, sortAtribute, orderMethod)    
+    
+    
+    const total_pages = response.total_pages
 
+    const container = document.getElementById("series_container")
+    container.innerHTML = ""
+
+    const data = response.data
+
+    data.forEach(serie => {
+        renderSerie(serie)
+    })
+    
+    const page_info = document.getElementById("page_info")
+    page_info.textContent = `Page ${actualPage} of ${total_pages}`
+
+    const prev_page = document.getElementById("prev_page")
+    prev_page.disabled = false
+    
+    if (actualPage === 1) {
+        prev_page.disabled = true
+    }
+    
+    const next_page = document.getElementById("next_page")
+    next_page.disabled = false
+    if (actualPage === response.total_pages){
+        next_page.disabled = true
+    }
+    } catch (error) {
+        console.error("Error loading series: ", error)
+        const container = document.getElementById("series_container")
+        container.innerHTML = `<p>Error loading series. Please try again.</p>`
+    }
+}
 
 const renderSerie = (serie) => {
     const serieCard = document.createElement('a')
+    const container = document.getElementById("series_container");
     serieCard.href = `series.html?id=${serie.id}`
 
     const img = document.createElement(`img`)
@@ -34,6 +71,6 @@ const renderSerie = (serie) => {
     serieCard.appendChild(platform);
     serieCard.appendChild(status);    
 
-    const container = document.getElementById("series_container");
+    
     container.appendChild(serieCard);
 }
