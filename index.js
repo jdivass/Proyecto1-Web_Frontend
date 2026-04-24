0d0edc1dc3752f6a018cf7f67013de2a3667d000
div468 <julianandre.divas@gmail.com>
2026-04-23 18:16:15 -0600

    Fixed sort frontend logic

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
 var actualPage = 1
 var limitPage = 10
 var searchTerm = ""
-var sortAtribute = "id"
-var orderMethod = "desc"
+var sortAtribute = document.getElementById("atributes").value
+var orderMethod = document.getElementById("order").value
 
 const loadSeries = async () => {
     try {
         const response = await getSeries(actualPage, limitPage, searchTerm, sortAtribute, orderMethod)    
-    
-    
-    const total_pages = response.total_pages
+        
+        const total_pages = response.total_pages
 
-    const container = document.getElementById("series_container")
-    container.innerHTML = ""
+        const container = document.getElementById("series_container")
+        container.innerHTML = ""
 
-    const data = response.data
+        const data = response.data
 
-    data.forEach(serie => {
-        renderSerie(serie)
-    })
-    
-    const page_info = document.getElementById("page_info")
-    page_info.textContent = `Page ${actualPage} of ${total_pages}`
+        data.forEach(serie => {
+            renderSerie(serie)
+        })
+        
+        const page_info = document.getElementById("page_info")
+        page_info.textContent = `Page ${actualPage} of ${total_pages}`
 
-    const prev_page = document.getElementById("prev_page")
-    prev_page.disabled = false
-    
-    if (actualPage === 1) {
-        prev_page.disabled = true
-    }
-    
-    const next_page = document.getElementById("next_page")
-    next_page.disabled = false
-    if (actualPage === response.total_pages){
-        next_page.disabled = true
+        const prev_page = document.getElementById("prev_page")
+        prev_page.disabled = false
+        
+        if (actualPage === 1) {
+            prev_page.disabled = true
+        }
+        
+        const next_page = document.getElementById("next_page")
+        next_page.disabled = false
+        if (actualPage === response.total_pages){
+            next_page.disabled = true
     }
     } catch (error) {
         console.error("Error loading series: ", error)
@@ -48,8 +47,10 @@ const renderSerie = (serie) => {
     serieCard.href = `series.html?id=${serie.id}`
 
     const img = document.createElement(`img`)
-    img.src = `${API_URL}/${serie.image_path}`
+    img.src = serie.image_path
     img.alt = serie.title
+    img.style.width = "300px"
+    img.style.height = "300px"
 
     const title = document.createElement("h2")
     title.textContent = serie.title;
